0cbdc98601506d7d334f972d841399efd99a5de5
SaburoDarame <profenyx@gmail.com>
2026-09-10 14:37:19 +0300

    Complete homework 21

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## Homework 17
+## Homework 21
 
 - script.js
--- js/script.js
+++ js/script.js
@@ -1,37 +1,82 @@
 "use strict";
 
-const padString = (str, num, sym, bool = false) => {
-  if (str === undefined) {
-    return "Error: str is missing";
+const product = [
+  { id: 31, name: "Mouse", price: 25, inStock: true },
+  { id: 2, name: "Keyboard", price: 70, inStock: false },
+  { id: 3, name: "Monitor", price: 210, inStock: true },
+];
+
+const mok = product.map((item) => {
+  const { name, price, inStock } = item;
+  const stockStatus = inStock ? "In Stock" : "Out of Stock";
+  return `Name: ${name}, Price: $${price}, ${stockStatus}`;
+});
+
+console.log(mok);
+
+//-----------------------------
+
+const users = [
+  { id: 1, age: 17, active: true, email: "a@mail.com" },
+  { id: 2, age: 22, active: true, email: "b@spam.com" },
+  { id: 3, age: 30, active: false, email: "c@mail.com" },
+  { id: 4, age: 35, active: true, email: "d@mail.com" },
+  { id: 5, age: 40, active: true, email: "e@mail.com" },
+];
+
+const filteredUsers = users.filter(
+  (user) =>
+    user.active === true &&
+    user.age >= 18 &&
+    user.age <= 35 &&
+    !user.email.endsWith("@spam.com"),
+);
+
+console.log(filteredUsers);
+
+//-----------------------------
+
+const tx = [
+  { id: 1, category: "food", amount: 12 },
+  { id: 2, category: "food", amount: 8 },
+  { id: 3, category: "taxi", amount: 15 },
+  { id: 4, category: "books", amount: 20 },
+  { id: 5, category: "taxi", amount: 7 },
+];
+
+const groupedTx = tx.reduce((acc, curr) => {
+  const { category, amount } = curr;
+  if (!acc[category]) {
+    acc[category] = 0;
   }
+  acc[category] += amount;
+  return acc;
+}, {});
 
-  if (num === undefined) {
-    return "Error: num is missing";
-  }
+console.log(groupedTx);
 
-  if (sym === undefined) {
-    return "Error: sym is missing";
-  }
+//-----------------------------
 
-  let result = str;
+const orders = [
+  {
+    id: 101,
+    items: [
+      { sku: "A1", qty: 1 },
+      { sku: "C3", qty: 2 },
+    ],
+  },
+  { id: 102, items: [{ sku: "B2", qty: 1 }] },
+  {
+    id: 103,
+    items: [
+      { sku: "B2", qty: 3 },
+      { sku: "A1", qty: 1 },
+    ],
+  },
+];
 
-  if (num < str.length) {
-    return result.substring(0, num);
-  }
+const firstOrderItems = orders.find((order) =>
+  order.items.some((item) => item.sku === "B2"),
+);
 
-  while (result.length < num) {
-    if (bool === true) {
-      result = sym + result;
-    } else {
-      result = result + sym;
-    }
-  }
-
-  return result;
-};
-
-console.log(padString("hello", 8, "*"));
-console.log(padString("hello", 6, "*", true));
-console.log(padString("hello", 2, "*"));
-console.log(padString("hello", 8));
-console.log(padString("hello"));
+console.log(firstOrderItems);
